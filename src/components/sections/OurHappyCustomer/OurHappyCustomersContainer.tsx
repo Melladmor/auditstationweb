a94e70ff76c992c506acc2ee3200ed6581bcaeee
MelladMorshed29 <melladmorshed29@gmail.com>
2025-03-15 16:47:35 +0400

    commit for save

--- src/components/sections/OurHappyCustomer/OurHappyCustomersContainer.tsx
+++ src/components/sections/OurHappyCustomer/OurHappyCustomersContainer.tsx
@@ -5,11 +5,13 @@ import OurHappyCustomresCard from "./OurHappyCustomresCard";
 import { OurHappyCustomersI } from "./type";
 import Button from "@/components/Buttons/Button";
 import { useTranslations } from "next-intl";
+import { useRouter } from "next/navigation";
 
 type Props = {
   data: OurHappyCustomersI[];
 };
 export function OurHappyCustomersContainer({ data }: Props) {
+  const navigate = useRouter();
   const t = useTranslations();
   return (
     <div>
@@ -20,7 +22,10 @@ export function OurHappyCustomersContainer({ data }: Props) {
         }}
       />
       <div className="flex items-center justify-center xl:mt-[32px] lg:mt-[32px] md:mt-[25px] sm:mt-[20px] xs:mt-[20px]">
-        <Button title={t("seemore")} onClick={() => console.log("test")} />
+        <Button
+          title={t("seemore")}
+          onClick={() => navigate.push("ourhappycustomers")}
+        />
       </div>
     </div>
   );
